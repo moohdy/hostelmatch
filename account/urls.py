b501ac8ed1f28f7c4a4aff52d60aafd5e4375c30
Abdullahi Muhammed <muhammedu9ted4ever@gmail.com>
2018-09-20 12:35:25 +0100

    fifth commit: move project from django 1.8 to django 1.11
    i upgraded from django 1.8 to django 1.11 and updated the urls.py files in the project to suit django1.11 requirements

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -12,17 +13,17 @@ urlpatterns = [
     url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
     
     # registration urls: login/logout urls
-    url(r'^login/$','django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',{'template_name':'registration/logged_out.html',}, name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout,{'template_name':'registration/logged_out.html',}, name='logout'),
+    url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
 
     # registration urls: password urls (change password/password reset urls)
-    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(r'^password-change/$', auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password-reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
     
 
     ]
